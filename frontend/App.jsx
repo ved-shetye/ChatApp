90cd8d33c8248bcb2741dfc25c1bdab6a4572ebb
ved_shetye <vedbshetye03@gmail.com>
2024-12-12 23:32:08 +0530

    added screens

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,16 +3,12 @@ import {LogBox} from 'react-native';
 LogBox.ignoreLogs(['new NativeEventEmitter']);
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './components/Home';
+import StackNavigator from './navigation/StackNavigator';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StackNavigator/>
   );
 };
 
--- frontend/navigation/StackNavigator.jsx
+++ frontend/navigation/StackNavigator.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ChatScreen from '../screens/ChatScreen'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import ProfileScreen from '../screens/ProfileScreen'
+import LoginScreen from '../screens/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen'
+import PeopleScreen from '../screens/PeopleScreen'
+import { NavigationContainer } from '@react-navigation/native'
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
@@ -19,18 +26,84 @@ const StackNavigator = () => {
                         headerShown: false,
                         tabBarIcon:({focused}) => 
                             focused ? (
-
+                                <MaterialIcons
+                                    name="chat-bubble-outline"
+                                    size={30}
+                                    color="white"
+                                />
                             ) : (
-                                
+                                <MaterialIcons
+                                    name="chat-bubble-outline"
+                                    size={30}
+                                    color="#989898"
+                                />
                             )
-                    }}/>
+                    }}
+                />
+                <Tab.Screen 
+                    name="Profile" 
+                    component={ProfileScreen} 
+                    options={{
+                        tabBarStyle:{backgroundColor:"#101010"}, 
+                        headerShown: false,
+                        tabBarIcon:({focused}) => 
+                            focused ? (
+                                <Ionicons
+                                    name="person-outline"
+                                    size={30}
+                                    color="white"
+                                />
+                            ) : (
+                                <Ionicons
+                                    name="person-outline"
+                                    size={30}
+                                    color="#989898"
+                                />
+                            )
+                    }}
+                />
             </Tab.Navigator>
         )
     }
+
+    const AuthStack = () => {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Login" 
+                    component={LoginScreen} 
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen 
+                    name="Register" 
+                    component={RegisterScreen} 
+                    options={{headerShown: false}}
+                />
+            </Stack.Navigator>
+        )
+    }
+
+    function MainStack(){
+        return (
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name = "Main"
+                    component={BottomTabs}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen 
+                    name = "People"
+                    component={PeopleScreen}
+                    options={{headerShown: false}}
+                />
+            </Stack.Navigator>
+        )
+    }
+
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
+    <NavigationContainer>
+        <MainStack/>
+    </NavigationContainer>
   )
 }
 
